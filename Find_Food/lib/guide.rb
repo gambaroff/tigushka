5cbefaaf1cc68bd6439147ba5649b22b741afc8d
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2014-01-09 14:40:52 -0800

    Complete

--- Find_Food/lib/guide.rb
+++ Find_Food/lib/guide.rb
@@ -1,6 +1,12 @@
 require 'restaurant'
+require 'support/string_extend'
 class Guide
- 
+  class Config
+    @@actions = ['list', 'find', 'add', 'quit']
+    def self.actions
+      @@actions
+    end    
+  end
  
   def initialize(path=nil)
     # locate the restaurant text file at path
@@ -20,39 +26,119 @@ class Guide
   def launch!
     introduction
     # action loop
-    loop do
-      # what do you want to do? ( list, find, add, quit)
-      print "> "
-      user_response = gets.chomp
-      # do that action
-      result = do_action(user_response)
-      # repeat untill user quits
-      break if result == :quit
+    result = nil
+    until result == :quit
+      action, args = get_action
+      result = do_action(action, args)
     end
     conclusion
   end
+  
+  def get_action
+    action = nil
+    # Keep asking for user input until we get a valid action
+    until Guide::Config.actions.include?(action)
+      puts "Actions: " + Guide::Config.actions.join(", ")
+      print "> "
+      user_response = gets.chomp
+      args = user_response.downcase.strip.split(' ')
+      action = args.shift
+    end
+    return action, args
+  end
 
-  def do_action(action)
+  def do_action(action, args=[])
     case action
     when 'list'
-      puts "Listing..."
+      list(args)
     when 'find'
-      puts "Finding..."
+      keyword = args.shift
+      find(keyword)
     when 'add'
-      puts "Adding..."
+      add
     when 'quit'
       return :quit
     else
-      puts "\nI don't undestand that command.\n"
+      puts "\nI don't understand that command.\n"
+    end
+  end
+  
+  def list(args=[])
+    sort_order = args.shift
+    sort_order = args.shift if sort_order == 'by'
+    sort_order = "name" unless ['name', 'cuisine', 'price'].include?(sort_order)
+    
+    output_action_header("Listing restaurants")
+    
+    restaurants = Restaurant.saved_restaurants
+    restaurants.sort! do |r1, r2|
+      case sort_order
+      when 'name'
+        r1.name.downcase <=> r2.name.downcase
+      when 'cuisine'
+        r1.cuisine.downcase <=> r2.cuisine.downcase
+      when 'price'
+        r1.price.to_i <=> r2.price.to_i
+      end
+    end
+    output_restaurant_table(restaurants)
+    puts "Sort using: 'list cuisine' or 'list by cuisine'\n\n"
+  end
+  
+  def find(keyword="")
+    output_action_header("Find a restaurant")
+    if keyword
+      restaurants = Restaurant.saved_restaurants
+      found = restaurants.select do |rest|
+        rest.name.downcase.include?(keyword.downcase) || 
+        rest.cuisine.downcase.include?(keyword.downcase) || 
+        rest.price.to_i <= keyword.to_i
+     end
+      output_restaurant_table(found)
+    else
+      puts "Find using a key phrase to search the restaurant list."
+      puts "Examples: 'find italian', 'find Italian', 'find ita'\n\n"
+    end
+  end
+  
+  def add
+    output_action_header("Add a restaurant")
+    
+    restaurant = Restaurant.build_using_questions
+    
+    if restaurant.save
+      puts "\nRestaurant Added\n\n"
+    else
+      puts "\nSave Error: Restaurant not added\n\n"
     end
   end
 
   def introduction
     puts "\n\n<<<< Welcome to the Food Finder. >>>>\n\n" 
-    puts "This is an interative guide helping with decisions about food. \n\n"
+    puts "This is an interative guide helping with decisions. \n\n"
   end
 
   def conclusion
     puts  "\n\n<<<< Goodbye and Bon Appetit! >>>>\n\n" 
+  end
+  private
+        
+        def output_action_header(text)
+          puts "\n#{text.upcase.center(60)}\n\n"
+        end
+        
+        def output_restaurant_table(restaurants=[])
+          print " " + "Name".ljust(30)
+          print " " + "Cuisine".ljust(20)
+          print " " + "Price".rjust(6) + "\n"
+          puts "-" * 60
+          restaurants.each do |rest|
+          line =  " " << rest.name.titleize.ljust(30)
+          line << " " + rest.cuisine.titleize.ljust(20)
+          line << " " + rest.formatted_price.rjust(6)
+          puts line
+        end
+    puts "No listings found" if restaurants.empty?
+    puts "-" * 60
   end
 end
--- Find_Food/lib/restaurant.rb
+++ Find_Food/lib/restaurant.rb
@@ -1,11 +1,14 @@
+require 'support/number_helper'
 class Restaurant
-  
+  include NumberHelper
   
   @@filepath = nil
   def self.filepath=(path=nil)
     @@filepath = File.join(APP_ROOT, path)
   end
 
+ attr_accessor :name, :cuisine, :price
+ 
   def self.file_exists?
     # class should know if the restaurant file exists
     if @@filepath && File.exists?(@@filepath)
@@ -32,5 +35,51 @@ class Restaurant
   def self.saved_restaurants
     # read the restaurant file
     # return instance of restaurant
+    restaurants = []
+    if file_usable?
+      file = File.new(@@filepath, 'r')
+      file.each_line do |line|
+        restaurants << Restaurant.new.import_line(line.chomp)
+      end
+      file.close
+    end
+    return restaurants
+  end
+  def self.build_using_questions
+    args = {}
+    print "Restaurant name: "
+    args[:name] = gets.chomp.strip
+
+    print "Cuisine type: "
+    args[:cuisine] = gets.chomp.strip
+    
+    print "Average price: "
+    args[:price] = gets.chomp.strip
+    
+    return self.new(args)
+  end
+  
+  def initialize(args={})
+    @name = args[:name] || ""
+    @cuisine = args[:cuisine] || ""
+    @price = args[:price] || ""
+  end
+  
+  def import_line(line)
+    line_array = line.split("\t")
+    @name, @cuisine, @price = line_array
+    return self
+  end
+  
+  def save
+    return false unless Restaurant.file_usable?
+    File.open(@@filepath, 'a') do |file|
+      file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
+    end
+    return true
+  end
+  
+  def formatted_price
+    number_to_currency(@price)
   end
 end
